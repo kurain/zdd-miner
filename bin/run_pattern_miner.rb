c0fa763eb8658eab2245b678a5d879a29caa9dcc
Ryutaro Kurai <ryu.kurai@gmail.com>
2013-07-01 16:47:18 +0900

    montly test

--- bin/run_pattern_miner.rb
+++ bin/run_pattern_miner.rb
@@ -3,21 +3,27 @@ require 'vsop_utils.rb'
 
 include VSOPUtils
 
-pattern = ARGV[0] || 'HL*'
+pattern = ARGV[0] || 'L*HHH*L*'
 
 pm = PatternMiner.new(pattern, :debug => false, :lcm => "M")
 
 files = []
-(2008..2011).each do |year|
-  files.push "nico_data/#{year}.txt_fimi.txt"
-end
+dir = 'nico_month'
+files = Dir.glob(dir + '/*fimi.txt')
 
-warn files.inspect
-pm.read_lookup_file('nico_data/2007.txt_lookup.txt')
+warn "We will read #{files.length} files"
+
+order_file_path  = Dir.glob(dir + '/*_order.txt')[0]
+lookup_file_path = Dir.glob(dir + '/*lookup.txt')[0]
+
+warn order_file_path
+warn lookup_file_path
+
+pm.read_lookup_file(lookup_file_path)
 
 ratio = 0.0002
 files.each do |file|
-  zdd = pm.frequent_itemsets(file, ratio, 'nico_data/2007.txt_order.txt')
+  zdd = pm.frequent_itemsets(file, ratio, order_file_path)
   pm.accept_itemsets(zdd)
 end
 puts pm.symbol_to_name(pm.found_sets.to_s)
